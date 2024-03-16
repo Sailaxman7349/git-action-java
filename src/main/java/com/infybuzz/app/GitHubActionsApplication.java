d67891ad89176e871db5b99ffe88a37e037d8843
sailaxman7349 <sailaxman7349@gmail.com>
2024-03-16 06:44:03 +0530

    initial code

---
+++ src/main/java/com/infybuzz/app/GitHubActionsApplication.java
@@ -0,0 +1,20 @@
+package com.infybuzz.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GitHubActionsApplication {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Hello world";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubActionsApplication.class, args);
+	}
+
+}
